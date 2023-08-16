f1d48e214ebafed49916184a5da0eeab8d0a21cc
chezkin <hynymn@gmail.com>
2023-08-16 17:10:24 +0300

    Almost everything is perfect

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
-
-const express = require('express');
-const fs = require('fs');
+import { emailval, passwordval } from './func.js';
+import express from 'express';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import jsonfile from 'jsonfile';
+// const EmailValidation = require('emailvalid')
+const saltRounds = 10;
+// const ev = new EmailValidation()
 
 const app = express();
 const port = 3000;
@@ -18,42 +24,94 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
-
-
 app.get('/users', (req, res) => {
-    fs.readFile('./data.json', function (err, data) {
+    jsonfile.readFile('./data.json', function (err, data) {
         if (err) throw err;
         res.send(data);
     });
 });
 
 app.get('/users/:id', (req, res) => {
-  
-    fs.readFile('./data.json', function (err, data) {
+
+    jsonfile.readFile('./data.json', function (err, data) {
         if (err) throw err;
-        const users = JSON.parse(data).users;
-        const userID = req.params.id;
-        const user = users.find((user) => user.id == userID);
-        res.send(user);
+        const users = JSON.parse(data);
+        users.forEach(user => {
+            if (user.id.slice(0, 3) == req.params.id) {
+                res.send(user);
+            }
+        });
+
     });
 });
 
 app.post('/add-user', (req, res) => {
-    fs.readFile('./data.json', function (err, data) {
+    jsonfile.readFile('./data.json', function (err, users) {
         if (err) throw err;
+        const uuid = uuidv4();
+        if (! passwordval(req.body.password)) {
+            res.send('invalid password');
+            return;
+        };
+        const myPlaintextPassword = req.body.password;
+        const myHash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+        if (! emailval(req.body.email)) {
+            res.send('invalid email')
+            return;
+        };
         const newUser = {
-            id: req.body.id,
+            id: uuid,
             email: req.body.email,
-            password: req.body.password,
-        } ;
-        const users = JSON.parse(data).users;
+            password: myHash,
+        };
         users.push(newUser);
-        const db = {users: users}
-        fs.writeFile('./data.json', JSON.stringify(db), (err) => {
+        jsonfile.writeFile('./data.json', users, (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
             res.send('The file has been saved!');
-            }); 
+        });
+    });
+});
+
+app.put('/edit-user/:id', (req, res) => {
+    jsonfile.readFile('./data.json', function (err, users) {
+        if (err) throw err;
+
+        const userID = users[req.params.id];
+        if (req.body.email) { userID.email = req.body.email }
+        if (req.body.password) { userID.password = req.body.password }
+        jsonfile.writeFile('./data.json', users, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            res.send('The file has been saved!');
+        });
+    });
+});
+
+app.delete('/delete-user', (req, res) => {
+    jsonfile.readFile('./data.json', function (err, users) {
+        if (err) throw err;
+        const newUsers = users.filter(user => user.id != req.body.id);
+        jsonfile.writeFile('./data.json', newUsers, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            res.send('The file has been saved!');
+        });
+    });
+});
+
+app.post('/if-user', (req, res) => {
+    jsonfile.readFile('./data.json', function (err, users) {
+        if (err) throw err;
+        users.forEach(user => {
+            if (user.email == req.body.email) {
+
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    res.send("User is connected");
+                }
+            }
+        });
+        res.send("wrong credentials")
     });
 });
 
--- func.js
+++ func.js
@@ -1,9 +1,32 @@
-// import file from './data.json';
-// import fs from 'fs';
 
-export function readAllUsers() {
+import validator from "email-validator"
+
+
+
+import passwordValidator from 'password-validator';
+const schema = new passwordValidator();
+schema
+.is().min(6)                                    // Minimum length 8
+.is().max(10)                                  // Maximum length 100
+// .has().uppercase()                              // Must have uppercase letters
+// .has().lowercase()                              // Must have lowercase letters
+// .has().digits(2)                                // Must have at least 2 digits
+// .has().not().spaces()                           // Should not have spaces
+// .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
+
+function readAllUsers() {
      fs.readFile('./data.json', function(err, data) {
         if (err) throw err;
         res.send(data);
     });
 }
+
+
+export function emailval(email) {
+   return validator.validate(email) ? true : false;
+}
+
+export function passwordval(password) {
+    return schema.validate(password) ? true : false;
+ }
